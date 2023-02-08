2c07b7d176fa6e745bd2da7972e1780fafd74241
SakRoi <roininensaku@gmail.com>
2023-02-08 16:26:40 +0200

    added a enemy.py file to hold the enemy class

---
+++ enemy.py
@@ -0,0 +1,7 @@
+
+class EnemyBaseInfo:
+
+    def __init__ (self, health=10: int, damage=2: int, AI_difficulty = "easy": str) -> None:
+        self.health = health
+        self.difficulty = AI_difficulty
+        self.damage = damage
