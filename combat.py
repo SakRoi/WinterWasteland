2c7555b63c87c51fca7465d186db0f0d150d9f62
SakRoi <roininensaku@gmail.com>
2023-02-08 15:35:47 +0200

    added the combat system

---
+++ combat.py
@@ -0,0 +1,16 @@
+
+class CombatSystem:
+    """A defination of the games combat system, is basically Rock Paper Scissors"""
+
+    def __init__(self, player_character, enemy_entity) -> None:
+        self.player = player_character
+        self.enemy = enemy_entity
+
+    def _easy_combat_AI(self):
+        pass
+    
+    def _medium_combat_AI(self):
+        pass
+
+    def _hard_combat_AI(self):
+        pass
