2daf1a57f8aef98fd8197271818c1ec02d07e3db
SakRoi <roininensaku@gmail.com>
2023-02-22 15:19:42 +0200

    Feat: Add dictionaries for combat mechanics and random number generator

--- combat.py
+++ combat.py
@@ -1,3 +1,4 @@
+import random
 
 class CombatSystem:
     """A defination of the games combat system, is basically Rock Paper Scissors"""
@@ -7,7 +8,19 @@ class CombatSystem:
         self.player = player_character
         self.enemy = None
         self.last_player_choice = None
-    
+
+        self.combat_win_dictionary = {
+            "Rock":"Scissor",
+            "Scissor":"Paper",
+            "Paper":"Rock"
+        }
+        
+        self.random_combat_ai_dictionary = {
+            1:"Rock",
+            2:"Paper",
+            3:"Scissor"
+        }
+
     def combat(self, enemy_entity) -> None:
         """A callable method for enemy encounters in the wasteland"""
         self.enemy = enemy_entity
@@ -15,29 +28,8 @@ class CombatSystem:
             player_choice = self._player_combat_choice()
             enemy_choice = self._enemy_combat_choice(self.last_player_choice)
 
-            #check who wins and deal damage to the loser or both if draw, this right now is very simple
-            print(f"The enemy chose {enemy_choice}!")
-            print(f"You chose {player_choice}!")
-
-            #TODO make this smarter and refactor!!!
-            #Win
-            if player_choice == "Paper":
-                print("you hit the enemy!")
-                self.enemy.enemy_health -= 1
-            #Loss
-            elif player_choice == "Scissor":
-                print("The enemy hits you!")
-                self.player.player_health -= self.enemy.enemy_damage
-            #Draw
-            elif player_choice == "Rock": 
-                print("You both get scratched!")
-                self.player.player_health -= self.enemy.enemy_damage//2
-                self.enemy.enemy_health -= 1
             
-            print(f"Player health: {self.player.player_health}")
-            print(f"Enemy health: {self.enemy.enemy_health}")
-            self.last_player_choice = player_choice
-            #
+            
         print("You've slain the enemy!")
         self.enemy = None
         self.last_player_choice = None
@@ -63,10 +55,20 @@ class CombatSystem:
         return choice
     
     def _easy_combat_AI(self, last_player_choice: str) -> str:
-        """Easy combat AI"""
-        #always returns rock for testing purposes
-        #return self._random_combat_AI()
-        return "Rock"
+        """
+        Easy combat AI helper method. Always tries to counter
+        the player's move. E.g. If player chose paper last
+        round, chooses scissors this round
+        """
+
+        # due to the combat_win_dictionary being win-lose and not lose-win,
+        # we need to input last_losing_move back into the dictionary and use
+        # it's value as the move
+
+        last_losing_move = self.combat_win_dictionary[last_player_choice]
+        current_predicted_winning_move = self.combat_win_dictionary[last_losing_move]
+
+        return current_predicted_winning_move
     
     def _medium_combat_AI(self, last_player_choice: str) -> str:
         """
@@ -80,5 +82,33 @@ class CombatSystem:
     def _random_combat_AI(self) -> str:
         """Always returns a random choice from RPS"""
         
-        #set to only return rock for testing purposes
-        return "Rock"
+        random_number = random.randint(1, 3)
+        print(random_number)
+        random_choice = self.random_combat_ai_dictionary[random_number]
+        
+        return random_choice
+
+    def _hit_checker(self, player_choice: str, enemy_choice: str,) -> None:
+        """check who wins and deal damage to the loser or both if draw,"""
+
+        print(f"The enemy chose {enemy_choice}!")
+        print(f"You chose {player_choice}!")
+
+        #TODO make this smarter and refactor!!!
+        #Win
+        #if player_choice == "Paper":
+        #    print("you hit the enemy!")
+        #    self.enemy.enemy_health -= 1
+        #Loss
+        #elif player_choice == "Scissor":
+        #    print("The enemy hits you!")
+        #    self.player.player_health -= self.enemy.enemy_damage
+        #Draw
+        #elif player_choice == "Rock": 
+        #    print("You both get scratched!")
+        #    self.player.player_health -= self.enemy.enemy_damage//2
+        #    self.enemy.enemy_health -= 1
+            
+        #print(f"Player health: {self.player.player_health}")
+        #print(f"Enemy health: {self.enemy.enemy_health}")
+        #self.last_player_choice = player_choice
--- winterwasteland.py
+++ winterwasteland.py
@@ -1,12 +1,18 @@
+import random
+
 from player import PlayerInfo
 from enemy import EnemyInfo
 from combat import CombatSystem
 
 if __name__ == "__main__":
+    #set up the random seed generator for the game
+    random.seed()
+
     playerCharacter = PlayerInfo()
     playerCharacter.set_player_name()
     playerCharacter.take_player_heritage_choice()
     playerCharacter.print_player_character_template()
+    
     test_enemy = EnemyInfo()
     test_combat = CombatSystem(playerCharacter)
     test_combat.combat(test_enemy)
